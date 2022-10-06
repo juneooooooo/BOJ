495b69f674709e731d507604324e602cc2fe154c
juneooooooo <101615043+juneooooooo@users.noreply.github.com>
2022-10-06 12:37:16 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 224 ms, Memory: 18256 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -9,22 +9,12 @@ public class Main {
 		//StringBuilder sb = new StringBuilder();
 		
 		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
 		
-		int[] arr = new int[150];
-		Arrays.fill(arr, -1);
+		String str = sc.next();
 		
-		String s = sc.next();
-		
-		for(int i = 0; i < s.length(); i++) {
-			if(arr[s.charAt(i)] == -1)
-				arr[s.charAt(i)] = i;
+		for(char i = 'a'; i <= 'z'; i++) {
+			System.out.print(str.indexOf(i) + " ");
 		}
-		for(int i = 'a'; i <= 'z'; i++) {
-			sb.append(arr[i] + " ");
-		}
-		
-		System.out.println(sb.toString());
 	}
 	
 }
