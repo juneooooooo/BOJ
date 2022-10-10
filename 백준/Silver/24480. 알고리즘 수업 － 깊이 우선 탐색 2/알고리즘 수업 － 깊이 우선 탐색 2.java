4e4576089600988d61092fdbd6a715619fcde8e9
juneooooooo <101615043+juneooooooo@users.noreply.github.com>
2022-10-10 23:08:24 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 1012 ms, Memory: 89212 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static boolean[] visited;
+	static ArrayList<Integer>[] edges;
+	static int[] answer;
+	static int N, M, R;
+	static int cnt = 1;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken()); // 정점
+		M = Integer.parseInt(st.nextToken()); // 간선
+		R = Integer.parseInt(st.nextToken()); // 시작 정점
+		
+		visited = new boolean[N + 1];
+		answer = new int[N + 1];
+		
+		edges = new ArrayList[N + 1];
+		for(int i = 1; i <= N; i++) 
+			edges[i] = new ArrayList<>();
+		
+		int u = 0;
+		int v = 0;
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			u = Integer.parseInt(st.nextToken());
+			v = Integer.parseInt(st.nextToken());
+			
+			edges[u].add(v);
+			edges[v].add(u);
+		}
+		
+		for(int i = 1; i <= N; i++) // 인접 정점은 오름차순으로 방문하기 위해 오름차순 정렬
+			Collections.sort(edges[i], Collections.reverseOrder());
+		
+		DFS(R);
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1; i <= N; i++) 
+			sb.append(answer[i]).append("\n");
+		
+		System.out.println(sb);
+		
+		//bw.flush();
+		//bw.close();
+		//br.close();
+	}
+	
+	static void DFS(int v) {	
+		answer[v] = cnt++;
+		visited[v] = true;
+		
+		for(int next : edges[v]) {
+			if(!visited[next]) {
+				DFS(next);
+			}
+		}
+	}
+}
