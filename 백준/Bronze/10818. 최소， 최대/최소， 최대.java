d1dd8072996f1efdbbf085933aa0ab65bc1d17aa
juneooooooo <101615043+juneooooooo@users.noreply.github.com>
2022-10-11 15:17:17 +0900

    [Bronze III] Title: 최소, 최대, Time: 1344 ms, Memory: 107828 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		
+		System.out.println(arr[0] + " " + arr[N - 1]);
+		
+		//bw.flush();
+		//bw.close();
+		br.close();
+	}
+}
