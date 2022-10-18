534faa2372a3dff989094ad6321dd54ff5965c6b
juneooooooo <101615043+juneooooooo@users.noreply.github.com>
2022-10-18 23:38:43 +0900

    [Gold IV] Title: 최단경로, Time: 756 ms, Memory: 107320 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -17,11 +17,17 @@ Arrays.sort(time, new Comparator<int[]>() {
 		//	양수일 경우 : 두 원소의 위치를 교환 함
 */
 
-class node {
+class node implements Comparable<node> {
 	int dist, idx;
-	node(int dist, int idx) {
+	
+	public node(int dist, int idx) {
 		this.dist = dist;
 		this.idx = idx;
+	}
+	
+	@Override
+	public int compareTo(node n) {
+		return this.dist - n.dist;
 	}
 }
 
@@ -37,12 +43,14 @@ public class Main {
 		
 		// 다익스트라 : 간선의 가중치가 있는 경우 한 시작점에서 다른 지점들까지 각각의 최단경로를 구하는 알고리즘.
 		int[] dist = new int[V + 1]; // start부터 [idx] 까지의 최단거리
-		boolean[] visited = new boolean[V + 1]; // 정점 방문여부
+		
 		ArrayList<node>[] map = new ArrayList[V + 1];
 		for(int i = 1; i <= V; i++)
 			map[i] = new ArrayList<>();
 		
 		Arrays.fill(dist, Integer.MAX_VALUE); // 최단거리 테이블 초기화
+		
+		PriorityQueue<node> pq = new PriorityQueue<>();
 		
 		for(int i = 0; i < E; i++) { // map[a][b] == 0이면 연결 X
 			st = new StringTokenizer(br.readLine());
@@ -54,26 +62,24 @@ public class Main {
 		}
 		
 		dist[start] = 0;
+		pq.add(new node(0, start));
 		
-		int min = 0, cur = 1;
-		for(int i = 1; i <= V; i++) {
-			min = Integer.MAX_VALUE;
+		while(!pq.isEmpty()) {
+			node n = pq.poll();
 			
-			// 방문하지 않은 정점 중에, 현재 정점(i)에서 가장 가까운 정점(j) 찾기
-			for(int j = 1; j <= V; j++) {
-				if(!visited[j] && dist[j] < min) {
-					min = dist[j];
-					cur = j; // 가장 가까운 정점
-				}
-			}
+			// 현재 노드가 이미 처리된 적이 있다면 무시
+			if(dist[n.idx] < n.dist)
+				continue;
 			
-			visited[cur] = true;
-			
-			for(int j = 0; j < map[cur].size(); j++) {
-				node next = map[cur].get(j);
+			// 현재 노드와 연결된 다른 인접한 노드들을 확인
+			for(node next : map[n.idx]) {
+				int cost = n.dist + next.dist;
 				
-				if(dist[next.idx] > dist[cur] + next.dist)
-					dist[next.idx] = dist[cur] + next.dist;
+				// 현재 노드를 거쳐서, 다음 노드로 이동하는 거리가 더 짧은 경우
+				if(cost < dist[next.idx]) {
+					dist[next.idx] = cost;
+					pq.add(new node(cost, next.idx));
+				}
 			}
 		}
 		
