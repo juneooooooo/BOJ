06cb558bc1348a9f8699ff429e8b2ef0833065f1
juneooooooo <101615043+juneooooooo@users.noreply.github.com>
2022-10-20 01:17:43 +0900

    [Silver I] Title: 카드 구매하기, Time: 152 ms, Memory: 14488 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+/*
+Arrays.sort(time, new Comparator<int[]>() {
+		
+		@Override
+		public int compare(int[] a, int[] b) {
+			if(a[1] == b[1]) {
+				return a[0] - b[0];
+			}
+			
+			return a[1] - b[1];
+		}
+	}); //  [두 수의 비교 결과에 따른 작동 방식]
+		//	음수일 경우 : 두 원소의 위치를 교환 안함
+		//	양수일 경우 : 두 원소의 위치를 교환 함
+*/
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] cards = new int[N + 1];
+		int[] dp = new int[N + 1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= N; i++)
+			cards[i] = Integer.parseInt(st.nextToken());
+		
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= i; j++) {
+				dp[i] = Math.max(dp[i], dp[i - j] + cards[j]);
+			}
+		}
+		System.out.println(dp[N]);
+	}
+	
+}
