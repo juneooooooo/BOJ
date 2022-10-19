d566ff7606bea47dbcdfa5accb6504c04c49d9e9
juneooooooo <101615043+juneooooooo@users.noreply.github.com>
2022-10-19 19:55:44 +0900

    [Silver III] Title: 2×n 타일링, Time: 128 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+/*
+Arrays.sort(time, new Comparator<int[]>() {
+		
+		@Override
+		public int compare(int[] a, int[] b) {
+			if(a[1] == b[1]) {
+				return a[0] - b[0];
+			}
+			
+			return a[1] - b[1];
+		}
+	}); //  [두 수의 비교 결과에 따른 작동 방식]
+		//	음수일 경우 : 두 원소의 위치를 교환 안함
+		//	양수일 경우 : 두 원소의 위치를 교환 함
+*/
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[1001];
+		
+		dp[1] = 1;
+		dp[2] = 2;
+		
+		for(int i = 3; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+			dp[i] %= 10007;
+		}
+		
+		System.out.println(dp[n]);
+	}
+}
