94853f2253301142777538eee61097ac0f0236e5
juneooooooo <101615043+juneooooooo@users.noreply.github.com>
2022-10-21 20:10:31 +0900

    [Silver III] Title: N과 M (7), Time: 788 ms, Memory: 263868 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+
+/*
+Arrays.sort(time, new Comparator<int[]>() {
+		
+		@Override
+		public int compare(int[] a, int[] b) {
+			if(a[1] == b[1]) {
+				return a[0] - b[0];
+			}
+			
+			return a[1] - b[1];
+		}
+	}); //  [두 수의 비교 결과에 따른 작동 방식]
+		//	음수일 경우 : 두 원소의 위치를 교환 안함
+		//	양수일 경우 : 두 원소의 위치를 교환 함
+*/
+
+public class Main {
+	static int[] arr, result;
+	static boolean[] visited;
+	static int N, M;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N + 1];
+		visited = new boolean[N + 1];
+		result = new int[M + 1];
+		sb = new StringBuilder();
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= N; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(arr);
+		
+		DFS(0);
+		
+		System.out.println(sb);
+	}
+	
+	static void DFS(int depth) {
+		if(depth == M) {
+			for(int i = 0; i < M; i++) {
+				sb.append(result[i] + " ");
+			}
+			sb.append("\n");
+			
+			return;
+		}
+		
+		for(int i = 1; i <= N; i++) {
+			result[depth] = arr[i];
+			DFS(depth + 1);
+		}
+	}
+}
